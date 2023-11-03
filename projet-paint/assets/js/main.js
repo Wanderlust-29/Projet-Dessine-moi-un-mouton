3e4ccfee4af0610242df41f7ee0f5e97f0f60f04
Wanderlust-29 <ryanroudaut8@gmail.com>
2023-11-03 11:28:50 +0000

    bonus 3

--- projet-paint/assets/js/main.js
+++ projet-paint/assets/js/main.js
@@ -20,30 +20,37 @@ function getSelectedColor()
 function loadPalette(palette)
 {
     let colors = palette;
-    let divHeader = document.querySelectorAll("header > div");
+    let divHeader = document.querySelectorAll("header > div > input");
     
     for (let i = 0; i < colors.length; i++) {
-        divHeader[i].style.backgroundColor = colors[i];
+        divHeader[i].value = colors[i];
     }
 }
 
 
 window.addEventListener("DOMContentLoaded", function(){
-    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
-
+    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80", "#dd6e42", "#e8dab2", "#4f6d7a", "#c0d6df", "#CC978E", "#261447", "#CCF5AC", "#D64045"]);
+    
+    let divHeader = document.querySelectorAll("header > div > input");
+    let mainDivs = document.querySelectorAll("main > div > div");
+    
+    
     addEventListener("click", ()=>{
-        let divHeader = document.querySelectorAll("header > div");
         for (let i = 0; i< divHeader.length; i++){
             selectColor(event);
             console.log(getSelectedColor());
         }
     })
 
-    let mainDiv =document.querySelectorAll("main div").foreach(function(div){
-        mainDiv.addEventListener("click", ()=>{
+    mainDivs.forEach(function(div){
+        div.addEventListener("click", ()=>{
             let color = getSelectedColor(); 
             if (color){
-                div.style.backgroundColor = color;
+                if (div.style.backgroundColor){
+                    div.style.backgroundColor = "";
+                } else {
+                    div.style.backgroundColor = color;
+                }
             }
         })    
     })
