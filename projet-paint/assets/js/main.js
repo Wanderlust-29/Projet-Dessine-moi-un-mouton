ce268afc53273213d3637a16d377d0e0b11a68a5
Wanderlust-29 <ryanroudaut8@gmail.com>
2023-11-03 15:33:19 +0000

    Bonus 6

--- projet-paint/assets/js/main.js
+++ projet-paint/assets/js/main.js
@@ -1,4 +1,3 @@
-
 function selectColor(event)
 {
     let colorBox = event.target;
@@ -23,37 +22,69 @@ function loadPalette(palette)
     let divHeader = document.querySelectorAll("header > div > input");
     
     for (let i = 0; i < colors.length; i++) {
-        divHeader[i].value = colors[i];
+        divHeader[i].setAttribute("value", colors[i]);
+        divHeader[i].addEventListener("input", selectColor);
     }
 }
 
+function generateGrid(rows, cols) {
+    let mainDiv = document.querySelector("main");
+
+    mainDiv.innerHTML = '';
+
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < cols; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+        mainDiv.appendChild(row);
+    }
+}
 
 window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80", "#dd6e42", "#e8dab2", "#4f6d7a", "#c0d6df", "#CC978E", "#261447", "#CCF5AC", "#D64045"]);
     
-    let divHeader = document.querySelectorAll("header > div > input");
+    let inputHeader = document.querySelectorAll("header > div > input");
     let mainDivs = document.querySelectorAll("main > div > div");
-    
-    
-    addEventListener("click", ()=>{
-        for (let i = 0; i< divHeader.length; i++){
-            selectColor(event);
-            console.log(getSelectedColor());
-        }
-    })
 
-    mainDivs.forEach(function(div){
-        div.addEventListener("click", ()=>{
-            let color = getSelectedColor(); 
-            if (color){
-                if (div.style.backgroundColor){
-                    div.style.backgroundColor = "";
-                } else {
-                    div.style.backgroundColor = color;
-                }
-            }
-        })    
-    })
     
-
+    for (let i = 0; i< inputHeader.length; i++){
+        inputHeader[i].addEventListener("input", ()=>{
+            let colorInput = inputHeader[i].value;
+            console.log(colorInput);
+            sessionStorage.setItem("selectedColor", colorInput);
+            
+            mainDivs.forEach(function(div){
+                div.addEventListener("click", ()=>{
+                    if (colorInput){
+                        if (div.style.backgroundColor){
+                            div.style.backgroundColor = "";
+                        } else {
+                            div.style.backgroundColor = colorInput;
+                        }
+                    }
+                })    
+            })        
+            
+        })
+    }
+    
+    // let gridRows = document.getElementById("gridRows");
+    // let gridColumns = document.getElementById("gridColumns");
+    // let rows = document.createElement("div")
+    
+    // gridRows.addEventListener("input", ()=>{
+    //     let rows = parseInt(gridRows.value);
+    //     let cols = parseInt(gridColumns.value);
+    //     generateGrid(rows, cols);
+    // });
+    
+    // gridColumns.addEventListener("input", function() {
+    //     let rows = parseInt(gridRows.value);
+    //     let cols = parseInt(gridColumns.value);
+    //     generateGrid(rows, cols);
+    // });
 });
